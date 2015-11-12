da5f1bda7e2cfeb510573bd956148748b772004b
Sen Geronimo <sen.geronimo@gmail.com>
2015-11-12 21:13:55 +0000

    added comments to problems 1 and 2

--- problem1.js
+++ problem1.js
@@ -1,9 +1,12 @@
 var i;
 var x = 0;
 
+/* loops until i is no longer less than 100 */
 for (i = 1; i < 1000; i++) {
+    /* takes any number divisible by 3 and adds it to x */
     if (i % 3 === 0) {
         x += i;
+    /* takes any number divisible by 5 and adds it to x */
     } else if (i % 5 === 0) {
         x += i;
     }
--- problem2.js
+++ problem2.js
@@ -3,11 +3,20 @@ var y = 1;
 var z = 1;
 var sum;
 
+/*  loops until i is no longer less than 4 million  */
 for(var i = 0; i < 4000000; i++) {
+    /*  adds the inital values of y and z at the start of the loop
+        and then adds then subsituted values at the end of each loop    */
     x = y + z;
+    
+    /*  determines whether the sum of the 2 numbers are 
+        even, if it is, it adds it to the total of the sum variable */
+        
 	if(x % 2 === 0 && x < 4000000) {
 		sum +=x;
 	}
+	/*  switches the values of y with z and z with x so it can be sent
+	    to the top of the loop  */
 	y = z;
 	z = x;
 }
